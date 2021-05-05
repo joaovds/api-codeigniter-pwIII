cee840ace9753209d46d0d9a929458cdaccc6d07
João Victor da Silva <jv782063@gmail.com>
2021-05-05 15:42:07 -0300

    add bootstrap

--- application/views/home.php
+++ application/views/home.php
@@ -6,9 +6,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/bootstrap/css/bootstrap.min.css">
+
     <title>Home</title>
   </head>
   <body>
-    <h1>Hello</h1>
+    <h2>Hello <span class="badge bg-info">2</span></h2>
+
+    <script src="<?= base_url() ?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
